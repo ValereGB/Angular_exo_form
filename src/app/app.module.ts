9e275acfa2b2b4d804b3e19828bb023202dc3010
Valere GB <valeregbv1@gmail.com>
2021-09-01 14:33:56 +0200

    Projet Formulaire fini

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+const routes: Routes = [
+
+  { path: "", pathMatch: "full", redirectTo: "login"},
+  
+  { path: "login", component: LoginComponent },
+  
+  { path: "admin", component: AdminComponent }
+  
+  ];
+  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
